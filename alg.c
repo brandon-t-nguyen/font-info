10ad97fc459c1c5c94707e1e0e0b171f6b77581b
Brandon Nguyen <brandontrinhnguyen@gmail.com>
2016-11-11 03:39:28 -0600

    Added a parameterized CalcAll that will run for all chars

--- alg.c
+++ alg.c
@@ -34,30 +34,72 @@ static double Alg_calculate( const B_Image image, B_Mask masks[], int numConvs )
     finalScore = finalScore >= 1.0 ? 1.0 : finalScore;
     return finalScore;
 }
+
+// wrapper for doing operations that require entire glyph set
+static double Alg_CalcAll( Algorithm alg, const BT_Face face, double (*algorithm)( Algorithm, const B_Image ) )
+{
+    double scoreTotal = 0.0;
+    int num = 0;
+    for( int c = FIRST_GLYPH; c <= LAST_GLYPH; c++ )
+    {
+        const B_Image image = BT_Face_getChar( face, c );
+        if( image )
+        {
+            scoreTotal += algorithm( alg, image );
+            ++num;
+        }
+    }
+    double scoreAverage = scoreTotal/num;
+    return scoreAverage;
+}
+
 /////// Dimensional /////
 static void Alg_WidthInit( Algorithm alg ){}
 static void Alg_WidthDone( Algorithm alg ){}
+static double Alg_WidthAlg( Algorithm alg, const B_Image image )
+{
+    double width = (double)B_Image_getWidth( image );
+    // normalize
+    return width / 1000;
+}
 static double Alg_WidthCalc( Algorithm alg, const BT_Face face )
 {
-    return 0.0;
+    return Alg_CalcAll( alg, face, &Alg_WidthAlg );
 }
 
 static void Alg_HeightInit( Algorithm alg ){}
 static void Alg_HeightDone( Algorithm alg ){}
+static double Alg_HeightAlg( Algorithm alg, const B_Image image )
+{
+    double height = (double)B_Image_getHeight( image );
+    // normalize
+    return height / 1000;
+}
 static double Alg_HeightCalc( Algorithm alg, const BT_Face face )
 {
-    return 0.0;
+    return Alg_CalcAll( alg, face, &Alg_HeightAlg );
 }
 
 static void Alg_AspectRatioInit( Algorithm alg ){}
 static void Alg_AspectRatioDone( Algorithm alg ){}
+static double Alg_AspectRatioAlg( Algorithm alg, const B_Image image )
+{
+    double width = (double)B_Image_getWidth( image );
+    double height = (double)B_Image_getHeight( image );
+    // TODO
+    return 0.0;
+}
 static double Alg_AspectRatioCalc( Algorithm alg, const BT_Face face )
 {
-    return 0.0;
+    return Alg_CalcAll( alg, face, &Alg_AspectRatioAlg );
 }
 
 static void Alg_xHeightInit( Algorithm alg ){}
 static void Alg_xHeightDone( Algorithm alg ){}
+static double Alg_xHeightAlg( Algorithm alg, const B_Image image )
+{
+    return 0.0;
+}
 static double Alg_xHeightCalc( Algorithm alg, const BT_Face face )
 {
     return 0.0;
@@ -67,39 +109,36 @@ static double Alg_xHeightCalc( Algorithm alg, const BT_Face face )
 /////// Density /////////
 static void Alg_DensityInit( Algorithm alg ){}
 static void Alg_DensityDone( Algorithm alg ){}
-static double Alg_DensityCalc( Algorithm alg, const BT_Face face )
+static double Alg_DensityAlg( Algorithm alg, B_Image image )
 {
-    double densityTotal = 0.0;
-    int num = 0;
-    for( int c = FIRST_GLYPH; c <= LAST_GLYPH; c++ )
+    int width = B_Image_getWidth( image );
+    int height = B_Image_getHeight( image );
+    int area = width * height;
+    int calc = 0;
+    for( int r = 0; r < height; r++ )
     {
-        const B_Image image = BT_Face_getChar( face, c );
-        if( image )
+        for( int c = 0; c < width; c++ )
         {
-            int width = B_Image_getWidth( image );
-            int height = B_Image_getHeight( image );
-            int area = width * height;
-            int calc = 0;
-            for( int r = 0; r < height; r++ )
-            {
-                for( int c = 0; c < width; c++ )
-                {
-                    if( B_Image_getPixel( image, c, r ) > 128 )
-                        ++calc;
-                }
-            }
-            area = area>0?area:1;
-            double density = calc / area;
-            densityTotal += density;
-            ++num;
+            if( B_Image_getPixel( image, c, r ) > 128 )
+                ++calc;
         }
     }
-    return densityTotal / num;
+    area = area>0?area:1;
+    double density = (double)calc / (double)area;
+    return density;
+}
+static double Alg_DensityCalc( Algorithm alg, const BT_Face face )
+{
+    return Alg_CalcAll( alg, face, &Alg_DensityAlg );
 }
 
 /////// Slant //////
 static void Alg_SlantInit( Algorithm alg ){}
 static void Alg_SlantDone( Algorithm alg ){}
+static double Alg_SlantAlg( Algorithm alg, const B_Image image )
+{
+    return 0.0;
+}
 static double Alg_SlantCalc( Algorithm alg, const BT_Face face )
 {
     return 0.0;
@@ -129,24 +168,17 @@ static void Alg_CurveDone( Algorithm alg )
         B_Mask_delete( alg->curve[i] );
     }
 }
+static double Alg_CurveAlg( Algorithm alg, const B_Image image )
+{
+    // get an edge detected image
+    B_Image edge = B_Mask_convolve( alg->edge, image );
+    double score = Alg_calculate( edge, alg->curve, NUM_CURVE_MASKS );
+    B_Image_delete(edge);
+    return score;
+}
 static double Alg_CurveCalc( Algorithm alg, const BT_Face face )
 {
-    double totalScore = 0.0;
-    int num = 0;
-    for( int c = FIRST_GLYPH; c <= LAST_GLYPH; c++ )
-    {
-        // get an edge detected image
-        const B_Image image = BT_Face_getChar(face,c);
-        if( image )
-        {
-            B_Image edge = B_Mask_convolve( alg->edge, image );
-            totalScore += Alg_calculate( edge, alg->curve, NUM_CURVE_MASKS );
-            B_Image_delete(edge);
-            ++num;
-        }
-    }
-    // average it
-    return totalScore/num;
+    return Alg_CalcAll( alg, face, &Alg_CurveAlg );
 }
 //////////////////////////////
 
